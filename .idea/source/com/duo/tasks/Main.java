b2b3d2cee22769eda7c1a310a41d49ddf94bfd47
roudder <roudderz@gmail.com>
2016-09-13 00:14:28 +0300

    3

--- .idea/source/com/duo/tasks/Main.java
+++ .idea/source/com/duo/tasks/Main.java
@@ -6,5 +6,6 @@ package com.duo.tasks;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Айнурка глупышка");
+        System.out.println("fsdsdfds");
     }
 }
